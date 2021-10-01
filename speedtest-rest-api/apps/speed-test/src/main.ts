b7d9dec1be1d4d9fa6bd92ec996448072fcb1e4d
Ben Ashby <mail@benashby.com>
2021-10-01 13:17:18 -0600

    added telegraf endpoint

--- speedtest-rest-api/apps/speed-test/src/main.ts
+++ speedtest-rest-api/apps/speed-test/src/main.ts
@@ -3,11 +3,28 @@ import {speedtestApi} from "@speedtest-rest-api/speedtest-api";
 
 const app = express();
 
-app.get('/api', async (req, res) => {
-  console.log(`${new Date().toISOString()} - Performing SpeedTest`)
+app.get('/all.json', async (req, res) => {
+  console.log(`${new Date().toISOString()} - Performing SpeedTest`);
   const result = await speedtestApi();
-  console.log(`${new Date().toISOString()} - Speedtest Finished`)
+  console.log(`${new Date().toISOString()} - Speedtest Finished`);
   res.send(result);
+});
+
+app.get('/telegraf.json', async (req, res) => {
+  console.log(`${new Date().toISOString()} - Performing SpeedTest`);
+  const result = await speedtestApi();
+  console.log(`${new Date().toISOString()} - Speedtest Finished`);
+  const mappedValue: any = {
+    'pingJitter': result.ping.jitter,
+    'pingLatency': result.ping.latency,
+    'upBandwidth': result.upload.bandwidth,
+    'upBytes': result.upload.bytes,
+    'upElapsed': result.upload.elapsed,
+    'downBandwidth': result.download.bandwidth,
+    'downBytes': result.download.bytes,
+    'downElapsed': result.download.elapsed
+  }
+  res.send(mappedValue);
 });
 
 const port = process.env.port || 3333;
